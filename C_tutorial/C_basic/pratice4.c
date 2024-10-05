f08c49ed837e0d54c89afc397dc4d07faf063322
anhbinhnguyen <anhbinhnguyen27042002@gmail.com>
2024-10-05 19:50:43 +0700

    Upload code and pratice skill

--- C_tutorial/C_basic/pratice4.c
+++ C_tutorial/C_basic/pratice4.c
@@ -1,20 +1,7 @@
 #include "stdio.h"
-#define LOWER 0
-#define UPPER 300
-#define STEP 20
+#include "stdlib.h"
 
 int main(){
-    float fahr,celius;
-        // while (fahr <=UPPER)
-        // {
-        //     /* Code program*/
-        //     celius = 5*(fahr-39)/9;
-        //     printf("%3d\t+%6d\n",fahr,celius);
-        //     fahr = fahr +STEP;
-        // }
-    for (LOWER;UPPER;fahr=fahr+STEP){
-        celius = 5*(fahr-39)/9;
-        printf("%.2f\t+%.2f\n",fahr,celius);
-    }
-    return 0;
+	
+	return 0;
 }
