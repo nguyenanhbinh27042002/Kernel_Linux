74480f57604475426ee0221da697490dc505f28a
anhbinhnguyen <anhbinhnguyen27042002@gmail.com>
2024-10-05 20:00:59 +0700

    Get perfect target

--- C_tutorial/C_basic/pratice4.c
+++ C_tutorial/C_basic/pratice4.c
@@ -1,7 +1,18 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+
 int main(){
+	double height, width;
+	printf("Enter the height \n");
+	scanf(" %lf",&height);
+
+	printf("Enter the width \n");
+	scanf("%lf",&width);
 	
+	double area;
+	area = height*width;
+	
+	printf("Area of the rectangle %lf \n",area);
 	return 0;
 }
