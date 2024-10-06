1cbbefca448d71077dcb36699c2f2bc8d2a3b25e
anhbinhnguyen <anhbinhnguyen27042002@gmail.com>
2024-10-06 17:03:08 +0700

    build code pratice_arithmetic

--- C_tutorial/C_basic/pratice_arithemic.c
+++ C_tutorial/C_basic/pratice_arithemic.c
@@ -1,8 +1,20 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+/* Formula : Sn = (a1+an)*n/2 */
 int main() {
-    float a1;
-    int d;
+    float a1,d,an,Sn;
+    int n;
+    printf("Enter the Initail term \n");
+    scanf("%f",&a1);
+    printf("Enter the difference in the Arithmetic Sequence: \n");
+    scanf("%f",&an);
+    printf("Enter the number of elements in the Arithmetic Sequence: \n");
+    scanf("%d",&n);
+
+    Sn = (a1+an)*n/2;
+    printf("Sum of your sequence %f\n",Sn);
+
+
     return 0;
 }
