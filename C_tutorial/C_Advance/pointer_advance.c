058282114f39875f19fc0a7b14a7acd43a5509a5
anhbinhnguyen <anhbinhnguyen27042002@gmail.com>
2024-10-09 21:36:22 +0700

    Update code and clean code

--- C_tutorial/C_Advance/pointer_advance.c
+++ C_tutorial/C_Advance/pointer_advance.c
@@ -1,23 +1,22 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-
-void findMaxMin(int num1, int num2, int *ptrMax, int *ptrMin){
-    if(num1>num2){
-        *ptrMax = num1;
-        *ptrMin = num2;
+void findMaxMin(int num1, int num2,int *ptrA, int *ptrB){
+    if (num1>num2) {
+        *ptrA = num1;
+        *ptrB = num2;
     }
     else {
-        *ptrMax = num2;
-        *ptrMin = num1;
-    } 
-
+        *ptrA = num2;
+        *ptrB = num1;
+    }
 }
 int main(){
-    int num1 = 10, num2= 30;
-    int max, min;
-    findMaxMin(num1,num2,&max,&min);
-    printf("The value of max between num1 and num2 %d \n",max);
-    printf("The value of min between num1 and num2 %d \n",min);
+    int a = 10, b = 20;
+    int max,min;
+    findMaxMin(a,b,&max,&min);
+    printf("The value of max %d \n",max);
+    printf("The value of min %d \n",min);
+
     return 0;
 }
