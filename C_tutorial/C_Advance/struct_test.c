91eaba156f0cae83e5f48ea63e8fe79357481344
anhbinhnguyen <anhbinhnguyen27042002@gmail.com>
2024-10-11 21:56:58 +0700

    Exercise 1

---
+++ C_tutorial/C_Advance/struct_test.c
@@ -0,0 +1,31 @@
+// #include "stdio.h"
+// #include "stdlib.h"
+
+// typedef struct point
+// {	
+// 	int x;
+// 	int y;
+// }Point;
+
+// int main()
+// {	
+// 	Point p1;
+// 	Point p2 = {5,7};
+// 	Point p3 = {.x = 3, .y = 5};
+// 	Point p4 = {.y = 10, .x =5};
+// 	Point p5 = {.x=10};
+
+// 	return 0;
+// }
+
+ /*Write the function 
+ 	- Function 1: get the point variable and print this information
+	- Function 2: responsible for getting a point input from the user and the return it*/
+#include "stdio.h"
+#include "stdlib.h"
+
+int main()
+{
+
+	return 0;
+}
