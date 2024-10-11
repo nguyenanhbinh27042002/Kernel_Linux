56c2d1fc39dfe17bf4c88b8a5466a4c704be81d1
anhbinhnguyen <anhbinhnguyen27042002@gmail.com>
2024-10-11 20:42:19 +0700

    update example for structs:

--- C_tutorial/C_Advance/struct.c
+++ C_tutorial/C_Advance/struct.c
@@ -1,13 +1,33 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+struct point {
+	int x;
+	int y;
+}; 
 
-int main(){
-	struct date{
+struct date
+{
+	/*struct in day for using cusomized data */
 	int day;
-	int month;
+	int months;
 	int year;
-}
-return 0;
+};
+
+int main()
+{
+	struct date graduteDate;
+	printf("Enter day:");
+	scanf("%d",&graduteDate.day);
+
+	printf("Enter months:");
+	scanf("%d",&graduteDate.months);
+
+	printf("Enter year:");
+	scanf("%d",&graduteDate.year);
+	printf("The my gradute day is (%d,%d,%d) \n",graduteDate.day,graduteDate.months,graduteDate.year);
+
+
+	return 0;
 }
 
