bebee126692203bdd403cf32ebd4b8dda1942a39
anhbinhnguyen <anhbinhnguyen27042002@gmail.com>
2024-10-12 21:19:38 +0700

    Create dynamic array

--- C_tutorial/C_Advance/dynamicArraystruct.c
+++ C_tutorial/C_Advance/dynamicArraystruct.c
@@ -8,16 +8,23 @@ typedef struct point {
 
 int main(){
 	Point pointArray[5];
-	printf("Enter the value first point\n");
+	printf("Enter the value first point x\n");
 	scanf("%d",&pointArray[0].x);
-	printf("Enter the value second point\n");
+	printf("Enter the value first point y\n");
 	scanf("%d",&pointArray[0].y);
 	
 	for(int i=0;i<5;i++)
 	{	
 		/*Enter code and innovate*/
+		printf("Enter the value point x string #%d \n",i+1);
+		scanf("%d",&pointArray[i].x);
+		printf("Enter the value point y string #%d \n",i+1);
+		scanf("%d",&pointArray[i].y);
 	}
-return 0;
+	for (int i=0;i<5;i++){
+		printf("Point #%d = (%d,%d)\n",i+1,pointArray[i].x,pointArray[i].y);
+	}
+	return 0;
 }
 
 
