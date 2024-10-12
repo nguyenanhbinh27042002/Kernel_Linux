ffa700234223e8a0580dca05dc51b36beb53b358
anhbinhnguyen <anhbinhnguyen27042002@gmail.com>
2024-10-12 20:27:08 +0700

    Pratice again

--- C_tutorial/C_Advance/exercise1.c
+++ C_tutorial/C_Advance/exercise1.c
@@ -7,30 +7,32 @@
 	Write the function :
 	Function 1:-get the function variable and prints it information
 	Function 2:-responsible for getting input from user and return it*/
+#include "stdio.h"
+#include "stdlib.h"
 
-typedef struct Point {
+typedef struct Point
+{
+	/* data for struct point x,y */
 	int x;
 	int y;
+}Point;
 
-}Point; 
-void printPoint(Point pt){
-	printf("The value of x %d\n",pt.x);
-	printf("The value of y:%d\n",pt.y);
+void printPoint(Point pt) {
+	printf("The value of x: %d\n",pt.x);
+	printf("The value of y: %d\n",pt.y);
 }
-
 Point pointview(){
 	Point pt;
-	printf("Enter the x \n");
+	printf("Enter the value of x:\n");
 	scanf("%d",&pt.x);
-	printf("Enter the y \n");
+	printf("Enter the value of y:\n");
 	scanf("%d",&pt.y);
-
 	return pt;
 }
 int main(){
-	Point pt1,pt2;
+	Point pt1;
 	pt1 = pointview();
 	printPoint(pt1);
 
-return 0;
+	return 0;
 }
