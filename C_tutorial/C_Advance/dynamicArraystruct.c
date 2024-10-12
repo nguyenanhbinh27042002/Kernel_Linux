0509fe1fdead806e65919af0d7fdf231e280a76b
anhbinhnguyen <anhbinhnguyen27042002@gmail.com>
2024-10-12 21:05:18 +0700

    Update code arrayPoiny

---
+++ C_tutorial/C_Advance/dynamicArraystruct.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+typedef struct point {
+	int x;
+	int y;
+}Point;
+
+int main(){
+	Point pointArray[5];
+	printf("Enter the value first point\n");
+	scanf("%d",&pointArray[0].x);
+	printf("Enter the value second point\n");
+	scanf("%d",&pointArray[0].y);
+	
+	for(int i=0;i<5;i++)
+	{	
+		/*Enter code and innovate*/
+	}
+return 0;
+}
+
+
